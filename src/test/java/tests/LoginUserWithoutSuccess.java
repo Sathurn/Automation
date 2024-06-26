cf34b041760f17e700569b7b24ab2ab13bb87be3
ECHISTISA <tiberiuchis@gmail.com>
2024-06-26 08:04:05 +0300

    added .idea to gitignore

--- src/test/java/tests/LoginUserWithoutSuccess.java
+++ src/test/java/tests/LoginUserWithoutSuccess.java
@@ -26,6 +26,7 @@ public class LoginUserWithoutSuccess extends BaseTest {
 
      //   Assert.assertEquals(login.getAccessForbidden(),"Access forbidden!");
 
+
         Assert.assertNotNull(login.getAccessForbidden());
 
     }
